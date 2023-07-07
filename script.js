23889c21c547fc95c13d5b66a3cfc5ef982a43fc
Matheus Lopes <m.lopeslemos96@gmail.com>
2023-07-07 15:49:48 -0300

    Site Pronto sobre Fake News

---
+++ script.js
@@ -0,0 +1,237 @@
+
+
+var contador = 0;
+var tentativas = 0;
+var numeroPerguntas = 19;
+
+function Placar() {
+  if (tentativas == numeroPerguntas){
+    if (contador == 1) {
+      
+      setTimeout(() => {  window.alert("Você acertou " + contador + " resposta"); }, 200);
+      
+    } else {
+      setTimeout(() => {  window.alert("Você acertou " + contador + " respostas"); }, 200);
+    }
+  }
+}
+
+
+function MudarEstado1(respostaCerta) {
+  if (respostaCerta) {
+    contador++;
+  }
+  tentativas++;
+  
+  document.getElementById("1").style.display = 'none';
+  document.getElementById("2").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado2(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("3").style.display = 'none';
+  document.getElementById("4").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado3(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("5").style.display = 'none';
+  document.getElementById("6").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado4(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("7").style.display = 'none';
+  document.getElementById("8").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado5(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("9").style.display = 'none';
+  document.getElementById("10").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado6(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("11").style.display = 'none';
+  document.getElementById("12").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado7(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("13").style.display = 'none';
+  document.getElementById("14").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado8(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("15").style.display = 'none';
+  document.getElementById("16").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado9(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("17").style.display = 'none';
+  document.getElementById("18").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado10(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("19").style.display = 'none';
+  document.getElementById("20").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado11(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("21").style.display = 'none';
+  document.getElementById("22").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado12(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("23").style.display = 'none';
+  document.getElementById("24").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado13(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("25").style.display = 'none';
+  document.getElementById("26").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado14(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("27").style.display = 'none';
+  document.getElementById("28").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado15(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("29").style.display = 'none';
+  document.getElementById("30").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado16(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("31").style.display = 'none';
+  document.getElementById("32").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado17(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("33").style.display = 'none';
+  document.getElementById("34").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado18(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("35").style.display = 'none';
+  document.getElementById("36").style.display = 'block';
+  Placar()
+}
+
+function MudarEstado19(respostaCerta) {
+
+  if (respostaCerta) {
+    contador++
+  }
+  tentativas++;
+  document.getElementById("37").style.display = 'none';
+  document.getElementById("38").style.display = 'block';
+  Placar()
+}
+
+
+
+
+
+
+
+
+
+
